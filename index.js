b4c93f9775aebbc0db50b35910d0d186f18e40f6
Dev <dev@singhk.dev>
2020-02-08 14:48:41 -0600

    try3

--- index.js
+++ index.js
@@ -9,11 +9,8 @@ const port = process.env.PORT || 80;
 app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-let mongo-options = {
-    server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
-    replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
-  };
-app.use(expressMongoDb('mongodb+srv://api-user-new:titanscout2022@2022-scouting-4vfuu.mongodb.net/test?retryWrites=true&w=majority', mongo-options));
+app.use(expressMongoDb('mongodb+srv://api-user-new:titanscout2022@2022-scouting-4vfuu.mongodb.net/test?retryWrites=true&w=majority', {server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }}));
 
 
 app.get('/', (req, res) => {
