b5302a2987d541ecbee8f6860e9d1c70fbb0546e
Dev <dev@singhk.dev>
2020-02-08 13:37:43 -0600

    k

--- dbHandler.js
+++ dbHandler.js
@@ -15,9 +15,13 @@ exports.addUserToTeam = (db, idin, namein, positionin) => {
     var dbo = db.db("data_scouting");
     var myobj = { id: idin, name: namein, position: positionin};
     dbo.collection("userlist").updateOne(myobj, {upsert:true}).then(function(err, res) {
-    console.log("1 document inserted");
-    db.close();
+        if (err){
+            console.error(err)
+        } else {
+            console.log("1 document inserted");
+        }
     });
+    db.close();
 }
 
 
