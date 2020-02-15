c09cca156c9ea77acc440dbf15891447ff73c2db
Dev <dev@singhk.dev>
2020-02-15 16:47:14 -0600

    dima

--- dbHandler.js
+++ dbHandler.js
@@ -60,7 +60,8 @@ exports.fetchMatchData = async (db, comp_idin, match_numberin, team_scoutedin) =
     data.err_occur = false
     console.log(comp_idin, match_numberin, team_scoutedin)
     var dbo = db.db("data_scouting");
-    var myobj = {competition: String(comp_idin), match: parseInt(match_numberin), team_scouted: parseInt(team_scoutedin)};
+    var myobj = {};
+    // var myobj = {competition: String(comp_idin), match: parseInt(match_numberin), team_scouted: parseInt(team_scoutedin)};
     console.log(myobj)
     try {
         data.data = await dbo.collection("matchdata").findOne(myobj).catch(e => {console.error(e);data.err_occur = true;})
