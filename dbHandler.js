6db13ad35fb5aada7a8a35be311792dc3445ed9f
Dev <dev@singhk.dev>
2020-02-18 19:38:01 -0600

    dima

--- dbHandler.js
+++ dbHandler.js
@@ -118,7 +118,8 @@ exports.addScouterToMatch = async (db, userin, matchin, team_scouted) => {
     try {
         let interim = await dbo.collection("matches").findOne(myobj).catch(e => {console.error(e);data.err_occur = true;})
         console.log("heres the interim data: ", interim)
-        console.log(interim.teams)
+        console.log(interim.teams[0])
+        console.log(team_scouted)
         let index = interim.teams.indexOf(team_scouted);
         console.log("heres the index", index)
         interim.scouters[index] = String(userin);
