f0c4593eeb83d2d4645d2a575cefd8b14fc31cf6
Jacob Levine <jacoblevine18@gmail.com>
2020-02-18 19:28:29 -0600

    added /api/fetchUID. plz test

--- dbHandler.js
+++ dbHandler.js
@@ -16,7 +16,7 @@
 // exports.getCompetitions = async (db, idin) => {
 //     let rval;
 //     idin = String(idin)
-//     // Get the competitions for a team member. Currently, one user can only be part of one team. 
+//     // Get the competitions for a team member. Currently, one user can only be part of one team.
 //     var dbo = db.db("data_scouting");
 //     var myobj = { id: idin};
 //     var data = await dbo.collection("userlist").findOne(myobj)
@@ -101,6 +101,25 @@ exports.fetchShotChartData = async (db, comp_idin, match_numberin, team_scoutedi
     let myobj = {competition: String(comp_idin), match: parseInt(match_numberin), team_scouted: parseInt(team_scoutedin)};
     try {
         data.data = await dbo.collection("shotchart").findOne(myobj).catch(e => {console.error(e);data.err_occur = true;})
+    } catch (err) {
+        data.err_occur = true
+        data.err_reasons.push(err)
+        console.error(err)
+    }
+    return data;
+}
+
+exports.fetchUIDsForMatch = async (db, comp_idin, match_numberin) => {
+    let data = {}
+    data.err_occur = false
+    data.err_reason= []
+    comp_idin = String(comp_idin)
+    let dbo = db.db("data_scouting");
+    let myobj = {competition: String(comp_idin), match: parseInt(match_numberin)};
+    //console.log(myobj)
+    try {
+         matchdata = await dbo.collection("matches").findOne(myobj).catch(e => {console.error(e);data.err_occur = true;};
+         data.data = matchdata.scouters
     } catch (err) {
         data.err_occur = true
         data.err_reasons.push(err)
