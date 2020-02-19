57ae6eedfdd9a2b3d73789fc6903fd9db4370e50
Dev <dev@singhk.dev>
2020-02-18 19:42:09 -0600

    help

--- dbHandler.js
+++ dbHandler.js
@@ -118,9 +118,12 @@ exports.addScouterToMatch = async (db, userin, matchin, team_scouted) => {
     try {
         let interim = await dbo.collection("matches").findOne(myobj).catch(e => {console.error(e);data.err_occur = true;})
         console.log("heres the interim data: ", interim)
-        console.log(interim.teams[0])
-        console.log(team_scouted)
-        let index = interim.teams.indexOf(team_scouted);
+        let index = interim.teams.indexOf(String(team_scouted));
+        if (index < 0) {
+            console.error("Does not exist")
+            data.err_occur = true
+            data.err_reasons.push("Team does not exist in scout schedule")
+        }
         console.log("heres the index", index)
         interim.scouters[index] = String(userin);
         console.log(interim)
