bf199dfa1ee9d8682fedf651f1e01c9383afb9be
Dev Singh <dev@singhk.dev>
2020-03-03 00:54:58 -0600

    god help me

--- authHandler.js
+++ authHandler.js
@@ -1,7 +1,6 @@
 const {OAuth2Client} = require('google-auth-library');
 
-CLIENT_ID = "291863698243-ofnqubd0fh5dqfhjo368c39uto1fmudt.apps.googleusercontent.com"
-
+CLIENT_ID = '291863698243-obu2fpbfpr7ul9db9lm7rmc1e4r3oeag.apps.googleusercontent.com'
 const client = new OAuth2Client(CLIENT_ID);
 module.exports.checkAuth = async (req, res, next) => {
   const ticket = await client.verifyIdToken({
