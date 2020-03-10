bab7eb92e9beee261ef57a456218ddd4e0dfe47e
Dev Singh <dev@singhk.dev>
2020-03-10 15:52:39 -0500

    memem

--- authHandler.js
+++ authHandler.js
@@ -3,7 +3,7 @@ const {OAuth2Client} = require('google-auth-library');
 CLIENT_ID = '291863698243-obu2fpbfpr7ul9db9lm7rmc1e4r3oeag.apps.googleusercontent.com'
 const client = new OAuth2Client(CLIENT_ID);
 module.exports.checkAuth = async (req, res, next) => {
-  const extUsers = ["Jon Abend", "Robyn Abend", "Dev Singh"]
+  const extUsers = ["Jon Abend", "Robyn Abend", "Dev Singh", "Jacob Levine", "Arthur Lu", "Ian Fowler"]
   const ticket = await client.verifyIdToken({
     idToken:   String(req.header('token')),
     audience: [CLIENT_ID, "291863698243-eg5i4fh001n7sl28b0bqgp4h2vae9gn2.apps.googleusercontent.com", "291863698243-ofnqubd0fh5dqfhjo368c39uto1fmudt.apps.googleusercontent.com", "291863698243-obu2fpbfpr7ul9db9lm7rmc1e4r3oeag.apps.googleusercontent.com", "291863698243-ovppseib28p6usahf60igsp7ia3ovq6l.apps.googleusercontent.com", "291863698243-0dsmvs8uetpd9odms7aqn63iknroi4op.apps.googleusercontent.com"]
