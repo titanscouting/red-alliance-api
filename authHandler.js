35022da50bd240e7cd952132e9afdaf6b2a3513e
Dev Singh <dev@singhk.dev>
2020-03-10 12:54:36 -0500

    add more flexible external support

--- authHandler.js
+++ authHandler.js
@@ -3,7 +3,7 @@ const {OAuth2Client} = require('google-auth-library');
 CLIENT_ID = '291863698243-obu2fpbfpr7ul9db9lm7rmc1e4r3oeag.apps.googleusercontent.com'
 const client = new OAuth2Client(CLIENT_ID);
 module.exports.checkAuth = async (req, res, next) => {
-  const extUsers = ["Jon Abend", "Robyn Abend"]
+  const extUsers = ["Jon Abend", "Robyn Abend", "Dev Singh"]
   const ticket = await client.verifyIdToken({
     idToken:   String(req.header('token')),
     audience: [CLIENT_ID, "291863698243-eg5i4fh001n7sl28b0bqgp4h2vae9gn2.apps.googleusercontent.com", "291863698243-ofnqubd0fh5dqfhjo368c39uto1fmudt.apps.googleusercontent.com", "291863698243-obu2fpbfpr7ul9db9lm7rmc1e4r3oeag.apps.googleusercontent.com", "291863698243-ovppseib28p6usahf60igsp7ia3ovq6l.apps.googleusercontent.com", "291863698243-0dsmvs8uetpd9odms7aqn63iknroi4op.apps.googleusercontent.com"]
@@ -13,7 +13,7 @@ module.exports.checkAuth = async (req, res, next) => {
 }).catch(err => {console.error(err); res.status(401);});
   try {
     const payload = ticket.getPayload();
-    if (payload['hd'] == 'imsa.edu' || extUsers.indexOf(payload['name']) > -1) {
+    if (payload['hd'] == 'imsa.edu' || extUsers.indexOf(payload['name']) > -1 || extUsers.indexOf(payload['sub']) > -1) {
       res.locals.id = payload['sub']
       res.locals.name = payload['name']
       console.log("Logged in " + res.locals.name + " with ID " + res.locals.id)
