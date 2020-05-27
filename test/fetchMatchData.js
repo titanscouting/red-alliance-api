8a39db62f8b7b0d50ab67dd200bdd66bfe2bdd44
Dev Singh <dev@devksingh.com>
2020-05-27 18:37:26 -0500

    fix unit tests to work with TS
    Signed-off-by: Dev Singh <dev@devksingh.com>

--- test/fetchMatchData.js
+++ test/fetchMatchData.js
@@ -3,7 +3,7 @@ process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const server = require('../index.js');
+const server = require('../built/index.js');
 
 const should = chai.should();
 
