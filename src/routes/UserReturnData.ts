8b21ce3da355d079162a0b26a10f8920493659f5
Dev Singh <dev@devksingh.com>
2020-05-29 12:33:24 -0500

    create a class for the data return from the API

---
+++ src/routes/UserReturnData.ts
@@ -0,0 +1,13 @@
+/**
+ * Is a data structure for data returns
+ */
+export default class UserReturnData {
+    data: any;
+    err_occur: boolean;
+    err_reasons: Array<string>
+    constructor() {
+      this.err_occur = false; 
+      this.data = {value: {}};
+      this.err_reasons = [];
+    }
+  }
