fd4bb5c88d5c766a134bce1a78b1584c6efad345
Dev Singh <dev@devksingh.com>
2020-05-29 12:33:30 -0500

    migrate unused functions

--- src/next-year.ts
+++ src/next-year.ts
@@ -37,3 +37,64 @@
 //     }
 //     res.json(resobj)
 // })
+// app.get('/api/fetchShotChartData', async (req: any, res:any) => {
+//   let val;
+//   const competitionID = String(req.body.competitionID);
+//   const matchNumber = parseInt(req.body.matchNumber, 10);
+//   const teamScouted = parseInt(req.body.teamScouted, 10);
+//   try {
+//     val = await dbHandler.fetchShotChartData(req.db, competitionID, matchNumber, teamScouted).catch((e) => { console.error(e); val.err_occur = true; });
+//   } catch (err) {
+//     console.error(err);
+//     val.err_occur = true;
+//   }
+//   let resobj = null;
+//   if (val.err_occur === false) {
+//     resobj = {
+//       success: true,
+//       competition: competitionID,
+//       matchNumber,
+//       teamScouted,
+//       data: val.data.data,
+//     };
+//   } else {
+//     resobj = {
+//       success: false,
+//       reasons: val.err_reasons,
+//     };
+//   }
+//   res.json(resobj);
+// });
+
+/**
+ * SHOT CHART STUFF (deemed unneccesary, unknown if functioning)
+ */
+
+// app.post('/api/submitShotChartData', auth.checkAuth, async (req: any, res:any) => {
+//   let val;
+//   const scouter = { name: String(res.locals.name), id: String(res.locals.id) };
+//   const competitionID = String(req.body.competitionID);
+//   const matchNumber = parseInt(req.body.matchNumber, 10);
+//   const teamScouted = parseInt(req.body.teamScouted, 10);
+//   const { data } = req.body;
+//   try {
+//     val = await dbHandler.submitShotChartData(req.db, scouter, competitionID, matchNumber, teamScouted, data).catch((e) => { console.error(e); val.err_occur = true; });
+//   } catch (err) {
+//     console.error(err);
+//     val.err_occur = true;
+//   }
+//   let resobj = null;
+//   if (val.err_occur === false) {
+//     resobj = {
+//       success: true,
+//       competition: competitionID,
+//       matchNumber,
+//     };
+//   } else {
+//     resobj = {
+//       success: false,
+//       reasons: val.err_reasons,
+//     };
+//   }
+//   res.json(resobj);
+// });
