595aba0c19b4b293bffa0fe17d6ab05658e0e3f8
Dev Singh <dev@devksingh.com>
2020-05-30 20:34:37 -0500

    chore: fix indent

--- src/routes/whoami.ts
+++ src/routes/whoami.ts
@@ -5,5 +5,5 @@ module.exports = (app: any, auth: any) => {
     app.get('/api/whoami', auth.checkAuth, async (req: any, res:any) => {
         let scouter: Scouter = {id: res.locals.id, name: res.locals.name};
         res.json({success: true, data: scouter})
-}
+    }
 }
