e92e36f3056c3ba8d290b3e3f808f8a6a181334f
Dev Singh <dev@devksingh.com>
2020-07-25 10:52:36 -0500

    remove unneccessary try...catch, use destructuring
    Signed-off-by: Dev Singh <dev@devksingh.com>

--- src/routes/getUserStrategy.ts
+++ src/routes/getUserStrategy.ts
@@ -3,15 +3,9 @@ import UserReturnData from './UserReturnData';
 module.exports = (app:any, dbHandler:any, auth: any) => {
   app.get('/api/getUserStrategy', auth.checkAuth, async (req: any, res:any) => {
     const val: UserReturnData = new UserReturnData();
-    const comp = String(req.query.competition);
-    const match = String(req.query.match_number);
-    const name = String(res.locals.name);
-    try {
-      val.data = await dbHandler.getUserStrategy(req.db, comp, match, name).catch((e) => { console.error(e); val.err_occur = true; });
-    } catch (err) {
-      console.error(err);
-      val.err_occur = true;
-    }
+    const { competition, match_number }: Record<string, string> = req.query;
+    const { name }: Record<string, string> = res.locals;
+    val.data = await dbHandler.getUserStrategy(req.db, competition, match_number, name).catch((e) => { console.error(e); val.err_occur = true; });
     // the try...catch is the next few lines serves to ensure the application doesn't just crash if scouters or teams were not returned by the DB handler.
     let dataInterim: Array<any>;
     try {
--- src/routes/submitMatchData.ts
+++ src/routes/submitMatchData.ts
@@ -18,21 +18,15 @@ module.exports = (app: any, dbHandler: any, auth: any) => {
   app.post('/api/submitMatchData', auth.checkAuth, async (req: any, res: any) => {
     let val: UserReturnData = new UserReturnData();
     const scouter: Scouter = { name: String(res.locals.name), id: String(res.locals.id) };
-    const competitionID = String(req.body.competitionID);
+    const { competitionID, data }: Record<string, string> = req.body;
     const matchNumber: number = parseInt(req.body.matchNumber, 10);
     const teamScouted: number = parseInt(req.body.teamScouted, 10);
-    const { data } = req.body;
-    try {
-      val = await dbHandler.submitMatchData(req.db, scouter,
-        competitionID, matchNumber, teamScouted, data).catch(
-        (e: string) => {
-          console.error(e); val.err_occur = true;
-        },
-      );
-    } catch (err) {
-      console.error(err);
-      val.err_occur = true;
-    }
+    val = await dbHandler.submitMatchData(req.db, scouter,
+      competitionID, matchNumber, teamScouted, data).catch(
+      (e: string) => {
+        console.error(e); val.err_occur = true;
+      },
+    );
     let resobj = null;
     if (val.err_occur === false) {
       resobj = {
