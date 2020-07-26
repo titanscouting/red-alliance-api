1bbcffddbdaa07494bbb223e58445c137c62af74
Dev Singh <dev@devksingh.com>
2020-07-26 11:09:38 -0500

    finally add all the proper status codes
    Signed-off-by: Dev Singh <dev@devksingh.com>

--- src/routes/submitMatchData.ts
+++ src/routes/submitMatchData.ts
@@ -1,6 +1,6 @@
 import Scouter from './Scouter';
 import UserReturnData from './UserReturnData';
-
+import StatusCodes from './StatusCodes';
 /**
  * POST route '/api/submitMatchData'
  * Allows the application to submit data to the API, with some key data seperated within the
@@ -21,25 +21,29 @@ module.exports = (app: any, dbHandler: any, auth: any) => {
     const { competitionID, data }: Record<string, string> = req.body;
     const matchNumber: number = parseInt(req.body.matchNumber, 10);
     const teamScouted: number = parseInt(req.body.teamScouted, 10);
+    if (!(matchNumber && teamScouted && competitionID && data)) {
+      res.status(StatusCodes.not_enough_info).json({
+        success: false,
+        reasons: ['A required parameter (match number, team scouted, competition ID, or data) was not provided'],
+      })
+    }
     val = await dbHandler.submitMatchData(req.db, scouter,
       competitionID, matchNumber, teamScouted, data).catch(
       (e: string) => {
         console.error(e); val.err_occur = true;
       },
     );
-    let resobj = null;
     if (val.err_occur === false) {
-      resobj = {
+      res.json({
         success: true,
         competition: competitionID,
         matchNumber,
-      };
+      });
     } else {
-      resobj = {
+      res.status(StatusCodes.no_data).json({
         success: false,
         reasons: val.err_reasons,
-      };
+      });
     }
-    res.json(resobj);
   });
 };
