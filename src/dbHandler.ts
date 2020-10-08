cb4b9907f0f5200a39968f517f3f394a86b1a174
Dev Singh <dev@devksingh.com>
2020-10-08 20:32:42 +0000

    refactor: finish modularization
    Signed-off-by: Dev Singh <dev@devksingh.com>

--- src/dbHandler.ts
+++ src/dbHandler.ts
@@ -17,64 +17,6 @@ export { default as addScouterToMatch } from './db-handlers/addScouterToMatch';
 export { default as removeScouterFromMatch } from './db-handlers/removeScouterFromMatch';
 export { default as submitStrategy } from './db-handlers/submitStrategy';
 export { default as fetchStrategy } from './db-handlers/fetchStrategy';
-
-export const fetchUserStrategy = async (db, compIdIn, matchIdIn, namein) => {
-  const data = { err_occur: false, err_reasons: [], data: {} };
-
-  const dbo = db.db('strategies');
-  const myobj = { competition: String(compIdIn), match: String(matchIdIn), scouter: String(namein) };
-  try {
-    data.data = await dbo.collection('data').find(myobj).toArray();
-  } catch (err) {
-    data.err_occur = true;
-    data.err_reasons.push(err);
-    console.error(err);
-  }
-  return data;
-};
-
-export const submitPitData = async (db, scouterin, competitionin, matchin, teamin, datain) => {
-  const data = { err_occur: false, err_reasons: [], data: {} };
-
-  const dbo = db.db('data_scouting');
-  const myobj = {
-    $set: {
-      scouter: scouterin, competition: competitionin, match: matchin, team_scouted: teamin, data: datain,
-    },
-  };
-  try {
-    await dbo.collection('pitdata').updateOne({ _id: competitionin + matchin + teamin }, myobj, { upsert: true }).catch((e) => { console.error(e); data.err_occur = true; });
-  } catch (err) {
-    data.err_occur = true;
-    data.err_reasons.push(err);
-    console.error(err);
-  }
-  return data;
-};
-
-export const fetchPitData = async (db, compIdIn, matchNumberIn, teamScoutedIn) => {
-  const data = { err_occur: false, err_reasons: [], data: {} };
-
-  const dbo = db.db('data_scouting');
-  // var myobj = {_id: compIdIn + teamScoutedIn + matchNumberIn};
-  const myobj = { competition: String(compIdIn), match: parseInt(matchNumberIn, 10), team_scouted: parseInt(teamScoutedIn, 10) };
-  try {
-    data.data = await dbo.collection('pitdata').findOne(myobj).catch((e) => { console.error(e); data.err_occur = true; });
-  } catch (err) {
-    data.err_occur = true;
-    data.err_reasons.push(err);
-    console.error(err);
-  }
-  return data;
-};
-// export const getCompetitions = async (db, idin) => {
-//     let rval;
-//     idin = String(idin)
-//     // Get the competitions for a team member. Currently, one user can only be part of one team.
-//     var dbo = db.db("data_scouting");
-//     var myobj = { id: idin};
-//     var data = await dbo.collection("userlist").findOne(myobj)
-//     return rval
-// }
-
-// const globalCompetition = '2020ilch';
+export { default as fetchUserStrategy } from './db-handlers/fetchUserStrategy';
+export { default as fetchPitData } from './db-handlers/fetchPitData';
+export { default as submitPitData } from './db-handlers/submitPitData';
