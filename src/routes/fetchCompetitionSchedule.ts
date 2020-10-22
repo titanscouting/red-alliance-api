bcdceb1922de59a91e9072e9827fa832795958be
Aubrey Hall <ahall@imsa.edu>
2020-10-22 01:10:31 +0000

    Enhancement: routes don't run without info
    Signed-off-by: Aubrey Hall <ahall@imsa.edu>

--- src/routes/fetchCompetitionSchedule.ts
+++ src/routes/fetchCompetitionSchedule.ts
@@ -13,8 +13,9 @@ module.exports = (app: any, dbHandler: any) => {
     if (!(competition)) {
       val.err_occur = true;
       val.err_reasons.push('A required parameter (competition ID) was not provided')
+    } else {
+      val.data = await dbHandler.fetchCompetitionSchedule(req.db, competition).catch((e) => { console.error(e); val.err_occur = true; return {}; });
     }
-    val.data = await dbHandler.fetchCompetitionSchedule(req.db, competition).catch((e) => { console.error(e); val.err_occur = true; return {}; });
     if (val.err_occur === false) {
       res.json({
         success: true,
