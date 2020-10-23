626406ced89dc57f1c62df714abe4f517fb32236
23ahall <72054300+23ahall@users.noreply.github.com>
2020-10-22 20:52:14 -0500

    Enhancement: doesn't run rest of endpoint without necessary info (#66)
    * feat: get pit config from database
    
    done with trainees to teach how to write a route. we need to get the real config into the DB at some point
    
    Signed-off-by: Dev Singh <dev@devksingh.com>
    
    * fix: datatypes on authentication
    
    also fixes error handling on auth
    
    Signed-off-by: Dev Singh <dev@devksingh.com>
    
    * feat: pull fetchMatchConfig from db
    
    Signed-off-by: Aubrey Hall <ahall@imsa.edu>
    
    * fix: vs code being annoying
    
    Signed-off-by: Aubrey Hall <ahall@imsa.edu>
    
    * chore: fix enviornment
    
    Signed-off-by: Aubrey Hall <ahall@imsa.edu>
    
    * fix: remove borked endpoint
    
    Signed-off-by: Dev Singh <dev@devksingh.com>
    
    * fix: imports
    
    Signed-off-by: Dev Singh <dev@devksingh.com>
    
    * Revert "fix: imports"
    
    This reverts commit 370d275be0642c99e26efefbf9b720b5151f35f6.
    
    * Revert "fix: remove borked endpoint"
    
    This reverts commit 52c9f3312da0314b290b2187f14ae16848271a46.
    
    * fix: issues
    
    Signed-off-by: Dev Singh <dev@devksingh.com>
    
    * refactor: change location of templates
    
    Signed-off-by: Dev Singh <dev@devksingh.com>
    
    * tests: ignore all
    
    Signed-off-by: Dev Singh <dev@devksingh.com>
    
    * fix: remove borked endpoint
    
    Signed-off-by: Dev Singh <dev@devksingh.com>
    
    * fix: config endpoints for compatibility
    
    Signed-off-by: Dev Singh <dev@devksingh.com>
    
    * Revert "fix: config endpoints for compatibility"
    
    This reverts commit c29e81c5dab2069e091d4912ec215fa182b94ebc.
    
    * tests: address correct property of tests
    
    Signed-off-by: Dev Singh <dev@devksingh.com>
    
    * fix: null issue for matchdata
    
    Signed-off-by: Aubrey Hall <ahall@imsa.edu>
    
    * add issues
    
    Signed-off-by: Dev Singh <dev@devksingh.com>
    
    * fix: bugs in fetchStrategy and fetchMatches
    
    Signed-off-by: Aubrey Hall <ahall@imsa.edu>
    
    * Enhancement: routes don't run without info
    
    Signed-off-by: Aubrey Hall <ahall@imsa.edu>
    
    * Enhancement: doesn't run route without data
    
    Signed-off-by: Aubrey Hall <ahall@imsa.edu>
    
    * fix: doesn't run try-catch when data isn't provided
    
    Signed-off-by: Aubrey Hall <ahall@imsa.edu>
    
    * fix: doesn't run try-catch when data isn't provided
    
    Signed-off-by: Aubrey Hall <ahall@imsa.edu>
    
    * Enhancement: doesn't run the endpoint if information is not provided
    
    Signed-off-by: Aubrey Hall <ahall@imsa.edu>
    
    * Enhancement: Doesn't run rest of route findTeamNickname without necessary info
    
    Signed-off-by: Aubrey Hall <ahall@imsa.edu>
    
    Co-authored-by: Dev Singh <dev@devksingh.com>

--- test/fetchMatches.js
+++ test/fetchMatches.js
@@ -24,7 +24,6 @@ describe('GET/api/fetchMatches', () => {
       chai.request(server)
       .get('/api/fetchMatches')
       .end((err, res) => {
-        
         res.should.have.status(404);
         res.body.should.have.property('success').equal(false);
         done();
