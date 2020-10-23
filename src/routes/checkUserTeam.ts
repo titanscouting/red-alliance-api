755b619d94169eefa203055cd142d56d92c0b7ad
23ahall <72054300+23ahall@users.noreply.github.com>
2020-10-22 20:54:19 -0500

    feat: checkUserTeam route (#65)
    Signed-off-by: Aubrey Hall <ahall@imsa.edu>
    
    Co-authored-by: Dev Singh <dev@devksingh.com>

---
+++ src/routes/checkUserTeam.ts
@@ -0,0 +1,26 @@
+import UserReturnData from '../UserReturnData';
+import StatusCodes from '../StatusCodes';
+
+module.exports = (app:any, auth: any) => {
+  app.get('/api/checkUserTeam', auth.checkAuth, async (req: any, res:any) => {
+    const val: UserReturnData = new UserReturnData();
+    const { id, team, name } = res.locals;
+    if (!(team)) {
+      val.err_occur = true;
+      val.err_reasons.push('Team not known for user');
+    }
+    if (val.err_occur === false) {
+      res.json({
+        success: true,
+        id,
+        team,
+        name,
+      });
+    } else {
+      res.status(StatusCodes.no_data).json({
+        success: false,
+        reasons: val.err_reasons,
+      });
+    }
+  })
+}
