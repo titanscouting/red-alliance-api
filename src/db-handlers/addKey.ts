91647d84480d4788dd98e47bd96f31fecaedc2ef
Dev Singh <dev@devksingh.com>
2020-10-26 14:58:53 -0500

    fix: get team number when creating api key
    Signed-off-by: Dev Singh <dev@devksingh.com>

--- src/db-handlers/addKey.ts
+++ src/db-handlers/addKey.ts
@@ -1,13 +1,13 @@
 import * as bcrypt from 'bcrypt';
 import UserReturnData from '../UserReturnData';
 
-export default async (db: any, clientID: string, clientKey: string): Promise<UserReturnData> => {
+export default async (db: any, clientID: string, clientKey: string, team: number): Promise<UserReturnData> => {
   const data: UserReturnData = { err_occur: false, err_reasons: [], data: {} };
   const dbo = db.db('userlist');
   const hashedClientKey = bcrypt.hashSync(clientKey, 12);
   const myobj = {
     $set: {
-      clientID, hashedClientKey,
+      clientID, hashedClientKey, team
     },
   };
   await dbo.collection('api_keys').updateOne({ _id: clientID }, myobj, { upsert: true }).catch((e) => { console.error(e); data.err_reasons.push(e); data.err_occur = true; });
--- src/routes/addAPIKey.ts
+++ src/routes/addAPIKey.ts
@@ -10,14 +10,22 @@ import StatusCodes from '../StatusCodes';
  */
 module.exports = (app, dbHandler, auth) => {
   app.post('/api/addAPIKey', auth.noAPIKey, auth.checkAuth, async (req: any, res:any) => {
+    const team: number = req.body.team;
+    if (!team){
+      req.status(StatusCodes.no_data).json({
+        success: false,
+        err_reasons: ["Team not provided"]
+      })
+    }
     const val: UserReturnData = new UserReturnData();
     const clientInfo = await uuidAPIKey.create();
-    val.data = await dbHandler.addKey(req.db, clientInfo.uuid, clientInfo.apiKey).catch((e) => { console.error(e); val.err_occur = true; });
+    val.data = await dbHandler.addKey(req.db, clientInfo.uuid, clientInfo.apiKey, team).catch((e) => { console.error(e); val.err_occur = true; });
     if (val.err_occur === false) {
       res.json({
         success: true,
         CLIENT_ID: clientInfo.uuid,
         CLIENT_SECRET: clientInfo.apiKey,
+        team: req.body.team
       });
     } else {
       res.status(StatusCodes.no_data).json({
