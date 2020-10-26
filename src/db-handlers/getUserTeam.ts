0434d6fc619a5fc226e58f18e4602138863b98aa
Dev Singh <dev@devksingh.com>
2020-10-26 12:50:02 -0500

    fix: get user assocs from correct DB collection

--- src/db-handlers/getUserTeam.ts
+++ src/db-handlers/getUserTeam.ts
@@ -4,6 +4,6 @@ export default async (db: any, id: string): Promise<UserReturnData> => {
   const data: UserReturnData = { err_occur: false, err_reasons: [], data: {} };
   const dbo = db.db('userlist');
   const myobj = { id };
-  data.data = await dbo.collection('associations').findOne(myobj).catch((e) => { console.error(e); data.err_occur = true; data.err_reasons.push(e); });
+  data.data = await dbo.collection('data').findOne(myobj).catch((e) => { console.error(e); data.err_occur = true; data.err_reasons.push(e); });
   return data;
 };
