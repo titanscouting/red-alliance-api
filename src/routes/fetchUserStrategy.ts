e4e5e81c4437ea27ed09e65492a7bd9e6ef6549e
Shaan Doshi <shaandoshi4@gmail.com>
2020-11-05 02:22:44 +0000

    feat: add express-validation
    Signed-off-by: Shaan Doshi <shaandoshi4@gmail.com>

--- src/routes/fetchUserStrategy.ts
+++ src/routes/fetchUserStrategy.ts
@@ -1,26 +1,28 @@
+import { validate, Joi } from 'express-validation';
 import UserReturnData from '../UserReturnData';
 import StatusCodes from '../StatusCodes';
 
-module.exports = (app:any, dbHandler:any, auth: any) => {
-  app.get('/api/fetchUserStrategy', auth.checkAuth, async (req: any, res:any) => {
+module.exports = (app:any, dbHandler:any) => {
+  const validation = {
+    query: Joi.object({
+      competition: Joi.string().required(),
+      matchNumber: Joi.string().required(),
+    }),
+  }
+  app.get('/api/fetchUserStrategy', validate(validation, { keyByField: true }, {}), async (req: any, res:any) => {
     const val: UserReturnData = new UserReturnData();
     const { competition, matchNumber }: Record<string, string> = req.query;
     const { name }: Record<string, string> = res.locals;
     let dataInterim: Array<any>;
-    if (!(competition && matchNumber)) {
+
+    val.data = await dbHandler.fetchUserStrategy(req.db, competition, matchNumber, name).catch((e) => { console.error(e); val.err_occur = true; });
+    // the try...catch is the next few lines serves to ensure the application doesn't just crash if scouters or teams were not returned by the DB handler.
+
+    try {
+      dataInterim = val.data.data;
+    } catch (e) {
       val.err_occur = true;
-      val.err_reasons.push('A required parameter (competition ID or match number) was not provided')
-    } else {
-      val.data = await dbHandler.fetchUserStrategy(req.db, competition, matchNumber, name).catch((e) => { console.error(e); val.err_occur = true; });
-      // the try...catch is the next few lines serves to ensure the application doesn't just crash if scouters or teams were not returned by the DB handler.
-
-      try {
-        dataInterim = val.data.data;
-      } catch (e) {
-        val.err_occur = true;
-      }
     }
-
     if (val.err_occur === false) {
       res.json({
         success: true,
