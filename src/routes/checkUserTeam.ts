d9eb4fb5a655b510fa00ea4ed7ba05a9d605b4a2
Dev Singh <dev@devksingh.com>
2020-11-07 02:03:39 +0000

    dont validate checkuserteam
    Signed-off-by: Dev Singh <dev@devksingh.com>

--- src/routes/checkUserTeam.ts
+++ src/routes/checkUserTeam.ts
@@ -3,12 +3,7 @@ import UserReturnData from '../UserReturnData';
 import StatusCodes from '../StatusCodes';
 
 module.exports = (app:any, auth: any) => {
-  const validation = {
-    query: Joi.object({
-      team: Joi.string().required(),
-    }),
-  }
-  app.get('/api/checkUserTeam', auth.checkAuth, validate(validation, { keyByField: true }, {}), async (req: any, res:any) => {
+  app.get('/api/checkUserTeam', auth.checkAuth, async (req: any, res:any) => {
     const val: UserReturnData = new UserReturnData();
     const { id, team, name } = res.locals;
     if (val.err_occur === false) {
