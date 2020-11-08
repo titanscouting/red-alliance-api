42d251d76359d2d1de6557876229bc88cfd7dc4e
Dev Singh <dev@devksingh.com>
2020-11-08 23:29:13 +0000

    fix linting
    Signed-off-by: Dev Singh <dev@devksingh.com>

--- src/routes/privacyPolicy.ts
+++ src/routes/privacyPolicy.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+
 module.exports = (app: any) => {
   app.get('/privacy-policy', (req: any, res:any) => {
     res.sendFile(path.join(__dirname, '/../../public/privacy-policy.pdf'));
