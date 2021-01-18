6e173c34c6564e988e4c7080f0fac7458983f033
Dev Singh <dsingh@imsa.edu>
2021-01-18 17:46:30 -0600

    fix variable naming
    Signed-off-by: Dev Singh <dsingh@imsa.edu>

--- src/routes/fetchMatchData.ts
+++ src/routes/fetchMatchData.ts
@@ -7,16 +7,16 @@ module.exports = (app: any, dbHandler: any) => {
     query: Joi.object({
       competition: Joi.string().required(),
       match: Joi.string().required(),
-      team_scouted: Joi.string().required(),
+      teamScouted: Joi.string().required(),
     }),
   }
   app.get('/api/fetchMatchData', validate(validation, { keyByField: true }, {}), async (req: any, res:any) => {
     let val: UserReturnData = new UserReturnData();
-    const competitionID = String(req.query.competition);
+    const competition = String(req.query.competition);
     const match = parseInt(req.query.match, 10);
-    const team_scouted = parseInt(req.query.team_scouted, 10);
+    const teamScouted = parseInt(req.query.teamScouted, 10);
     let dataInterim;
-    val = await dbHandler.fetchMatchData(req.db, competitionID, match, team_scouted).catch((e) => { console.error(e); val.err_occur = true; val.err_reasons.push(e); });
+    val = await dbHandler.fetchMatchData(req.db, competition, match, teamScouted).catch((e) => { console.error(e); val.err_occur = true; val.err_reasons.push(e); });
     // the try...catch is the next few lines serves to ensure the application doesn't just crash if scouters or teams were not returned by the DB handler.
     try {
       dataInterim = val.data;
@@ -26,9 +26,9 @@ module.exports = (app: any, dbHandler: any) => {
     if (val.err_occur === false) {
       res.json({
         success: true,
-        competition: competitionID,
+        competition,
         match,
-        team_scouted,
+        teamScouted,
         data: dataInterim.data,
       });
     } else {
