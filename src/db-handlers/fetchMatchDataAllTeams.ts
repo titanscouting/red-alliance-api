ab47e893cf9c32353c1a69c075f5b1a6f6d90146
Aubrey Hall <ahall@imsa.edu>
2021-01-21 02:51:02 +0000

    Debug
    Signed-off-by: Aubrey Hall <ahall@imsa.edu>

--- src/db-handlers/fetchMatchDataAllTeams.ts
+++ src/db-handlers/fetchMatchDataAllTeams.ts
@@ -5,22 +5,25 @@ export default async (db: any, compID: string): Promise<UserReturnData> => {
   const dbo = db.db('data_scouting');
   const myobj = { competition: String(compID) };
   try {
-    const out = [];
+    const out = {};
+    const teamsSeen = [];
     const toProcess = await dbo.collection('matchdata').find(myobj).toArray();
-    // console.log(toProcess.data.match)
-    //  console.log(toProcess.data["team_scouted"]);
     for (const element of toProcess) {
       //  console.log(element);
-      for (const group of toProcess) {
-        //  const temp = { teamNum: { matchNum: 0, data: null } };
-        if (element.team_scouted === group.team_scouted) {
-          //  console.log(true);
-          out.push({ teamNum: { matchNum: element.match, data: element.data } });
-        }
+      if (teamsSeen.indexOf(element.team_scouted) === -1) {
+        console.log('not previously seen team');
+        out[`${element.team_scouted}`] = [];
+        teamsSeen.push(element.team_scouted);
       }
+      console.log(element.team_scouted, element.match)
+      out[`${element.team_scouted}`].push({ data: toProcess.data, match: toProcess.match })
+      // for (const group of toProcess) {
+      //   if (element.team_scouted === group.team_scouted) {
+      //     out.push({ teamNum: { matchNum: element.match, data: element.data } });
+      //   }
+      // }
     }
     data.data = out;
-    console.log(out);
   } catch (err) {
     data.err_occur = true;
     data.err_reasons.push(err);
