f1165e13db559ce6d50c24b065fb395fd0bd9700
Dev Singh <dsingh@imsa.edu>
2021-03-18 12:00:01 -0500

    do not load entire swagger to memory
    Signed-off-by: Dev Singh <dsingh@imsa.edu>

--- src/routes/fetchAPIConfig.ts
+++ src/routes/fetchAPIConfig.ts
@@ -1,10 +1,12 @@
 import StatusCodes from "../StatusCodes";
+import path from 'path';
 
 module.exports = (app: any) => {
   app.get('/api/fetchAPIConfig', async (req: any, res:any) => {
     try {
-        const apiConfig = require('./swagger.json')
-        res.json(apiConfig);
+      res.header("Content-Type",'application/json');
+      const p = path.resolve('src/routes/swagger.json')
+      res.sendFile(p);
     } catch {
         res.status(StatusCodes.no_data).json({success: false, reasons:["Could not read Swagger definition file"]})
     }
