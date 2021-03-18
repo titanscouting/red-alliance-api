99101fcff11fc32fc6d06c008a9010072f05f7c1
Dev Singh <dsingh@imsa.edu>
2021-03-18 12:04:28 -0500

    test linter

--- src/routes/fetchAPIConfig.ts
+++ src/routes/fetchAPIConfig.ts
@@ -5,7 +5,7 @@ module.exports = (app: any) => {
   app.get('/api/fetchAPIConfig', async (req: any, res:any) => {
     try {
       res.header('Content-Type', 'application/json');
-      const p = path.resolve('src/routes/swagger.json')
+      const p = path.resolve('src/routes/swagger.json');
       res.sendFile(p);
     } catch {
       res.status(StatusCodes.no_data).json({ success: false, reasons: ['Could not read Swagger definition file'] })
