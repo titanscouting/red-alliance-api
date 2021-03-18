e6af4c7b191e4bfeaad52302d2b146b149babced
Dev Singh <dsingh@imsa.edu>
2021-03-18 12:00:31 -0500

    fix lint issues
    Signed-off-by: Dev Singh <dsingh@imsa.edu>

--- src/routes/fetchAPIConfig.ts
+++ src/routes/fetchAPIConfig.ts
@@ -1,15 +1,14 @@
-import StatusCodes from "../StatusCodes";
 import path from 'path';
+import StatusCodes from '../StatusCodes';
 
 module.exports = (app: any) => {
   app.get('/api/fetchAPIConfig', async (req: any, res:any) => {
     try {
-      res.header("Content-Type",'application/json');
+      res.header('Content-Type', 'application/json');
       const p = path.resolve('src/routes/swagger.json')
       res.sendFile(p);
     } catch {
-        res.status(StatusCodes.no_data).json({success: false, reasons:["Could not read Swagger definition file"]})
+      res.status(StatusCodes.no_data).json({ success: false, reasons: ['Could not read Swagger definition file'] })
     }
-
   });
 };
