2be8b33402fef4d7636cba7b0b569e3e107a4344
Dev Singh <dsingh@imsa.edu>
2021-03-24 21:43:11 -0500

    fix removeScouterFromMatch
    Signed-off-by: Dev Singh <dsingh@imsa.edu>

--- src/routes/removeScouterFromMatch.ts
+++ src/routes/removeScouterFromMatch.ts
@@ -14,7 +14,8 @@ module.exports = (app: any, dbHandler: any, auth: any) => {
     let val: UserReturnData = new UserReturnData();
     const match = parseInt(req.body.match, 10);
     const teamScouted: string = req.body.team_scouting;
-    val = await dbHandler.removeScouterFromMatch(req.db, match, teamScouted).catch((e) => { console.error(e); val.err_occur = true; });
+    const { competition } = req.body
+    val = await dbHandler.removeScouterFromMatch(req.db, match, teamScouted, competition).catch((e) => { console.error(e); val.err_occur = true; });
 
     if (val.err_occur === false) {
       res.json({
