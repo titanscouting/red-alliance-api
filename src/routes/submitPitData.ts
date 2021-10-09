955aef1a15d188d83befd82b120ea2cc0f2c760a
Dev Singh <dsingh@imsa.edu>
2021-10-09 10:03:15 -0500

    enforce ownership for pitdata collection

--- src/routes/submitPitData.ts
+++ src/routes/submitPitData.ts
@@ -6,7 +6,6 @@ import StatusCodes from '../StatusCodes';
 module.exports = (app: any, dbHandler: any, auth: any) => {
   const validation = {
     body: Joi.object({
-      matchNumber: Joi.number().required(),
       teamScouted: Joi.number().required(),
       competition: Joi.string().required(),
       data: Joi.object().required(),
@@ -14,18 +13,16 @@ module.exports = (app: any, dbHandler: any, auth: any) => {
   }
   app.post('/api/submitPitData', auth.checkAuth, validate(validation, { keyByField: true }, { allowUnknown: true }), async (req: any, res:any) => {
     let val: UserReturnData = new UserReturnData();
-    const scouter: Scouter = { name: String(res.locals.name), id: String(res.locals.id), team: res.locals.team };
+    const scouter: Scouter = { name: String(res.locals.name), id: String(res.locals.id), team: String(res.locals.team) };
     const competitionID = String(req.body.competitionID);
-    const matchNumber: number = parseInt(req.body.matchNumber, 10);
-    const teamScouted: number = parseInt(req.body.teamScouted, 10);
+    const teamScouted = String(req.body.teamScouted);
     const { data }: Record<string, any> = req.body;
-    val = await dbHandler.submitPitData(req.db, scouter, competitionID, matchNumber, teamScouted, data).catch((e) => { console.error(e); val.err_occur = true; });
+    val = await dbHandler.submitPitData(req.db, scouter, competitionID, teamScouted, data).catch((e) => { console.error(e); val.err_occur = true; });
     if (val.err_occur === false) {
       res.locals.io.sockets.emit(`${String(scouter.team)}_${competitionID}_newPitData`, {})
       res.json({
         success: true,
         competition: competitionID,
-        matchNumber,
       });
     } else {
       res.status(StatusCodes.no_data).json({
