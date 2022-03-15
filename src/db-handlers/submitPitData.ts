104df92b0e8169275bea81f6d4238a4b7ab8625c
Dev Singh <dsingh@imsa.edu>
2022-03-14 23:37:55 -0500

    fix submitPitData function

--- src/db-handlers/submitPitData.ts
+++ src/db-handlers/submitPitData.ts
@@ -1,16 +1,16 @@
 import UserReturnData from '../UserReturnData';
 import Scouter from '../Scouter';
 
-export default async (db: any, scouter: Scouter, competition: string, match: number, team_scouted: number, datain: Record<string, any>): Promise<UserReturnData> => {
+export default async (db: any, scouter: Scouter, competition: string, team_scouted: string, datain: Record<string, any>): Promise<UserReturnData> => {
   const data: UserReturnData = { err_occur: false, err_reasons: [], data: {} };
   const dbo = db.db('data_scouting');
   const myobj = {
     $set: {
-      scouter, competition, match, team_scouted, data: datain, owner: scouter.team,
+      scouter, competition, team_scouted, data: datain, owner: scouter.team,
     },
   };
   try {
-    await dbo.collection('pitdata').updateOne({ _id: competition + match + team_scouted }, myobj, { upsert: true }).catch((e) => { console.error(e); data.err_occur = true; });
+    await dbo.collection('pitdata').updateOne({ _id: competition + team_scouted }, myobj, { upsert: true }).catch((e) => { console.error(e); data.err_occur = true; });
   } catch (err) {
     data.err_occur = true;
     data.err_reasons.push(err.toString());
