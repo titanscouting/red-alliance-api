a3e6aebf3994b27c4fa44d5bda1e704a83cadd37
Dev Singh <dsingh@imsa.edu>
2022-03-30 12:25:54 -0500

    fix db

--- src/routes/fetchMetricsData.ts
+++ src/routes/fetchMetricsData.ts
@@ -19,7 +19,7 @@ module.exports = (app: any, dbHandler: any, auth: any) => {
         success: true,
         competition,
         team,
-        metrics: val.data.data.metrics,
+        metrics: val.data.metrics[competition],
       });
     } else {
       res.status(StatusCodes.no_data).json({
